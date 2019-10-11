72e8581bfc3b9f5650e1de597dcaff3a95585bca
lytter <andreaslytter@hotmail.com>
2019-10-11 06:16:56 +0200

    fix av flagor i gui

--- src/barefoot/sweepervariants/SweeperBot.java
+++ src/barefoot/sweepervariants/SweeperBot.java
@@ -18,6 +18,7 @@ public interface SweeperBot {
 
     /**
      * Implement to calculate the next action
+     * Do not make multiple calls to game.takeAutomatedAction(), only one.
      * @param playerRevealedBoard Double[][] illustrating the gameboard.
      * @param game an instance of the GUI
      */
